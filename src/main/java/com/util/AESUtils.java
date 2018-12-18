0f175d28a9203189458ecfe4c321adffa7a75d8f
zhang <568125122@qq.com>
2018-12-18 16:52:59 +0800

    c

---
+++ src/main/java/com/util/AESUtils.java
@@ -0,0 +1,63 @@
+package com.util;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by michaelli on 2017/7/6.
+ */
+public class AESUtils {
+
+
+    public static byte[] encryptAES(String content, String key)
+            throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, UnsupportedEncodingException,
+            InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+
+        byte[] byteContent = content.getBytes("UTF-8");
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+
+        // 指定加密的算法、工作模式和填充方式
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+        byte[] encryptedBytes = cipher.doFinal(byteContent);
+//        return new BASE64Encoder().encode(encryptedBytes);
+        return encryptedBytes;
+    }
+
+
+    public static byte[] decryptAES(String content, String key)
+            throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException,
+            IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        // base64 解码
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+        byte[] result = cipher.doFinal(Base64Util.decode(content));
+//        return new String(result, "UTF-8");
+        return result;
+    }
+
+
+//    public static void main(String[] args) throws Exception {
+//        String key = "g@q35425ERfgdfbh";
+//
+//        String encodeData = "nRf2Ofnr8iztxemjXuXLceuhC05ZzLuYDAs83gVYn+4H3rECjGuzAdLFHoXHicy9ENgkjfSkGIjdre5I3BbYJs304WfDv6RAywZJDNbcQF8ngAmeieI49xDO5jGUyQ4XEBy4LBqZQa7n0Kbf2cgEmG6kzlaZohk/tWkT87c5blhc6M9iiMkSaQJaESKSib8kkv2nM79f6ClPY9b8VgkkatiN9xYJYowmx/2vi4Ww+wKTpERVRYhYSIk+3H80ZsngXfVcoYoK/VjpkBTdHzNa9sfovi2oPFkzsKwqGS5aVLtzlXCK0wipA2z0WCiSiRwtGmdk7pT0I6OZQMS21jweNUVPjNp3avq2/MOrnQ49M6y4CJNeKq2vlnazx7j+XRjW51wXKUVUgB3k+LQVd/VtTLv8OhS5nvCWQYtBqDUTMIRXxo1OcUbDIE3WTjCk6RmNRqX9sVMhc4gFZOB0aRtPMqsawWkbFxudE6WZerpSpoEXLJQe0AyWquEM/MpkzcP3Bx6Pq3lisTqME7EK8ivSoK9c2y7iYBxecw4IMYfXbVXW3r08S60kb43NJW21YL9i";
+////        String encodeStr = FuluAESUtils.encryptAES("000",key);
+////        System.out.println("encode str:"+encodeStr);
+//        String content = new String(FuluAESUtils.decryptAES(encodeData, key), "utf-8");
+//        System.out.println("content:"+content);
+//    }
+}
--- src/test/java/testJunit.java
+++ src/test/java/testJunit.java
@@ -1,16 +1,77 @@
 import com.Application;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.util.AESUtils;
+import com.util.Base64Util;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ResourceUtils;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT ,classes = Application.class)
 public class testJunit {
 
     @Test
-    public void test1(){
-        System.out.println("test junit~");
+    public void test1() throws UnsupportedEncodingException {
+        byte[] qwertyuis1 = (new Base64()).decode("qwertyui");
+        byte[] qwertyuis2 = Base64.decodeBase64("qwertyui");
+        byte[] qwertyuis3 = Base64Util.decode("qwertyui");
+
+        byte[] Str2 = "qwer".getBytes("UTF-8");
+        byte[] Str1 = "qwer".getBytes("UTF-8");
+        System.out.println(Base64.encodeBase64(Str1));
+        System.out.println(new String (Str2));
+
+        System.out.println(qwertyuis1);
+        System.out.println(new String((new Base64()).encode(qwertyuis1)));
+        System.out.println(qwertyuis2);
+        System.out.println(new String(Base64.encodeBase64(qwertyuis1)));
+
+        System.out.println(qwertyuis3);
+        System.out.println(new String(Base64Util.encode(qwertyuis3)));
+
+        System.out.println(new String(Base64Util.encode((qwertyuis1))));
+    }
+
+
+    @Test
+    public void test2() throws NoSuchPaddingException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, IOException, InvalidKeyException {
+//        HashMap hashMap = new HashMap();
+//        hashMap.put("1",123);
+//        String z = Base64Util.encode(AESUtils.encryptAES(new ObjectMapper().writeValueAsString(hashMap),"1231231231231231"));
+//        System.out.println(z);
+//        String param = new String(AESUtils.decryptAES(z, "1231231231231231"), "utf-8");
+//        System.out.println(param);
+        URL url = ResourceUtils.getURL("classpath:static/123.txt");
+        System.out.println(url);
+        File url1 = ResourceUtils.getFile("classpath:static/123.txt");
+        System.out.println(url1);
+
+        File targetFile = new File("/home/zihexin/servers/pics/merchantimg/", "123.1");
+        if (!targetFile.isDirectory()) {
+            File f = new File("/home/zihexin/servers/pics/merchantimg/");
+            f.mkdirs();
+        } else {
+            targetFile.mkdirs();
+        }
+        Files.copy(url1.toPath(), targetFile.toPath());
     }
 }
 
