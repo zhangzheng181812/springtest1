3dc427cd012228e4d415e0bd8210f212ac7d96df
zhang <568125122@qq.com>
2019-02-12 15:40:02 +0800

    spring 动态设置httpstatus响应值

--- src/main/java/com/zz/TestController.java
+++ src/main/java/com/zz/TestController.java
@@ -3,7 +3,9 @@ package com.zz;
 
 import com.service.AsyncService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +29,13 @@ public class TestController {
             asyncService.doAsync(i);
         }
         System.out.println("123");
+    }
+
+    @RequestMapping({"/sayHello2"})
+    @ResponseStatus(HttpStatus.CREATED)
+    //HttpStatus.CREATED 常量值为201，标识 新增资源成功（insert）
+    public String SayHello2() {
+        return "hello ~~";
     }
 }
 
