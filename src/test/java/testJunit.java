fa2f504a3443774b08039e693d42191cd6dba117
11 <568125122@qq.com>
2022-04-04 13:36:37 +0800

    1

--- src/test/java/testJunit.java
+++ src/test/java/testJunit.java
@@ -34,20 +34,19 @@ public class testJunit {
         byte[] qwertyuis2 = Base64.decodeBase64("qwertyui");
         byte[] qwertyuis3 = Base64Util.decode("qwertyui");
 
-        byte[] Str2 = "qwer".getBytes("UTF-8");
-        byte[] Str1 = "qwer".getBytes("UTF-8");
-        System.out.println(Base64.encodeBase64(Str1));
-        System.out.println(new String (Str2));
+//        byte[] Str2 = "qwer".getBytes("UTF-8");
+//        byte[] Str1 = "qwer".getBytes("UTF-8");
+//        System.out.println(Base64.encodeBase64(Str1));
+//        System.out.println(new String (Str2));
 
         System.out.println(qwertyuis1);
         System.out.println(new String((new Base64()).encode(qwertyuis1)));
         System.out.println(qwertyuis2);
         System.out.println(new String(Base64.encodeBase64(qwertyuis1)));
-
         System.out.println(qwertyuis3);
-        System.out.println(new String(Base64Util.encode(qwertyuis3)));
+        System.out.println(Base64Util.encode(qwertyuis3));
 
-        System.out.println(new String(Base64Util.encode((qwertyuis1))));
+        System.out.println(Base64Util.encode((qwertyuis1)));
     }
 
 
