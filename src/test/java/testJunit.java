26a9e832cc7ebbb3afafb5ea6b6ccfc635189d64
admin <568125122@qq.com>
2022-05-27 19:15:36 +0800

    test branch

--- src/test/java/testJunit.java
+++ src/test/java/testJunit.java
@@ -37,7 +37,7 @@ public class testJunit {
         byte[] Str2 = "qwer".getBytes("UTF-8");
         byte[] Str1 = "qwer".getBytes("UTF-8");
         System.out.println(Base64.encodeBase64(Str1));
-        System.out.println(new String (Str2));
+//        System.out.println(new String (Str2));
 
         System.out.println(qwertyuis1);
         System.out.println(new String((new Base64()).encode(qwertyuis1)));
